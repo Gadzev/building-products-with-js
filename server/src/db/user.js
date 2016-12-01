f58c65b563eb45012def83c6829f5a2fb7e13f48
Gadzev <gadzevalexandar@openmailbox.org>
2016-12-01 21:06:38 +0100

    Remove email field from user for now

--- server/src/db/user.js
+++ server/src/db/user.js
@@ -1,9 +1,10 @@
 import {thinky} from './thinky';
 
 // Create a model - the table is automatically created
-export const User = thinky.createModel("User", {
-    email: thinky.type.string().required(),
+const User = thinky.createModel('User', {
     login: thinky.type.string().required(),
     password: thinky.type.string().required(),
     registrationDate: thinky.type.date().default(thinky.r.now()),
 });
+
+export {User as default};
