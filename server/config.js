ad41eccecece4a69ed28cbd8b38f6c5c1633f119
Gadzev <gadzevalexandar@openmailbox.org>
2016-12-06 00:33:50 +0100

    Send JWT back after login, add tests for it

--- server/config.js
+++ server/config.js
@@ -7,4 +7,5 @@ export const db = {
 export const auth = {
     passwordSalt: 'Gq0twQYeoP6YWZY7iBc!NyhVavauPHB5Q6jPU$LMzcxw@SM&y$udLVnmF0qu!%XR',
     sessionSecret: 'zXqGQtHii2r4CUtYrO4F7lb5xHpNlYvBhb4nQ87bDdSvxigNWtGtzxxdDtwM3pZl',
+    jwtSecret: 'kjgHeIWRY1nCu30LX4wf7bwBvsE2jgm6bSxImVg5d9UsVfNHifBy2pnWyEJyQZtT',
 };
