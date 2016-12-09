3f637f10eff5c06961ffb347305c33c8a3a3c174
Gadzev <gadzevalexandar@openmailbox.org>
2016-12-09 01:07:49 +0100

    Add question update route and tests for it

--- server/src/question/index.js
+++ server/src/question/index.js
@@ -1,12 +1,12 @@
 // our packages
 import get from './get';
 import create from './create';
-// import update from './update';
-// import delete from './delete';
+import update from './update';
+import deleteQuestion from './delete';
 
 export default (app) => {
     get(app);
     create(app);
-    // update(app);
-    // delete(app);
+    update(app);
+    deleteQuestion(app);
 };
--- server/src/util/asyncRequest.js
+++ server/src/util/asyncRequest.js
@@ -1,5 +1,9 @@
+/* eslint arrow-parens: 0 */
 import {logger} from './logger';
 
 export const asyncRequest = handler =>
     (req, res) =>
-        handler(req, res).catch(e => logger.error('error during request:', e));
+        handler(req, res).catch(e => {
+            logger.debug('error during request:', e);
+            res.status(400).send({error: e.toString()});
+        });
