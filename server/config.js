6eccb3665478a08b5d189e4c0affb827a32d9281
Gadzev <gadzevalexandar@openmailbox.org>
2016-12-10 18:47:45 +0100

    user ES5 syntx for config, allow overriding config values with env variables

--- server/config.js
+++ server/config.js
@@ -1,11 +1,14 @@
-export const db = {
-    host: 'localhost',
-    port: 28015,
+exports.db = {
+    host: process.env.EXPERTS_DB_URL || 'localhost',
+    port: process.env.EXPERTS_DB_PORT || 28015,
     db: 'expertsdb',
 };
 
-export const auth = {
-    passwordSalt: 'Gq0twQYeoP6YWZY7iBc!NyhVavauPHB5Q6jPU$LMzcxw@SM&y$udLVnmF0qu!%XR',
-    sessionSecret: 'zXqGQtHii2r4CUtYrO4F7lb5xHpNlYvBhb4nQ87bDdSvxigNWtGtzxxdDtwM3pZl',
-    jwtSecret: 'kjgHeIWRY1nCu30LX4wf7bwBvsE2jgm6bSxImVg5d9UsVfNHifBy2pnWyEJyQZtT',
+exports.auth = {
+    passwordSalt: process.env.EXPERTS_AUTH_PASSALT ||
+        'Gq0twQYeoP6YWZY7iBc!NyhVavauPHB5Q6jPU$LMzcxw@SM&y$udLVnmF0qu!%XR',
+    sessionSecret: process.env.EXPERTS_AUTH_SESSECRET ||
+        'zXqGQtHii2r4CUtYrO4F7lb5xHpNlYvBhb4nQ87bDdSvxigNWtGtzxxdDtwM3pZl',
+    jwtSecret: process.env.EXPERTS_AUTH_JWTSECRET ||
+        'kjgHeIWRY1nCu30LX4wf7bwBvsE2jgm6bSxImVg5d9UsVfNHifBy2pnWyEJyQZtT',
 };
